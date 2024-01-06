1bd83b9cb11367c27e82c3a82ee2900ada2fe885
Bartłomiej Sieczka <bartlomiej.sieczka@crunchingkoalas.com>
2024-01-06 16:14:49 +0100

    Added game_init and project.godot

---
+++ game_init.gd
@@ -0,0 +1,8 @@
+class_name Init extends Node
+
+func _ready():
+	#TODO: Do intital setup here
+	
+	#TODO: Set intial game state here, eg:
+	#Game.set_state(Game.menu_state, { "mode" : "endless", "starting_hand" : 5, "debug" : false })
+	pass
